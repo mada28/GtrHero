7021a0d917358111b20a1d502846c9cad7309177
Buddy-1cent <Buddy-1cent@192.168.0.103>
2014-10-17 15:48:52 +0200

    add enum colours

---
+++ src/guitarhero/models/Colours.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitarhero.models;
+
+/**
+ *
+ * @author Buddy-1cent
+ */
+public enum Colours {
+    RED,YELLOW,BLUE,ORANGE,GREEN,PUPRLE
+}
