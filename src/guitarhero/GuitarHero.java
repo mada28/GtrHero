102ed8df50234746057e23ad5bf3980abadccd6d
Buddy-1cent <Buddy-1cent@192.168.0.103>
2014-10-17 22:32:51 +0200

    prehravani mp3

--- src/guitarhero/GuitarHero.java
+++ src/guitarhero/GuitarHero.java
@@ -5,18 +5,42 @@
  */
 package guitarhero;
 
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.*;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.*;
+
 /**
  *
  * @author Buddy-1cent
  */
 public class GuitarHero {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-
+    /*  
+        JFXPanel fxPanel = new JFXPanel();
+        Media hit = new Media(new File("asdf.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.play();
+        System.out.println("Uz to hraje kemo!");
+    */
+    //S pouzitim dalsi knihovny jl 1.0.1.jar
+    try{
+        File song = new File("asdf.mp3");
+        FileInputStream a = new FileInputStream(song);
+        BufferedInputStream b = new BufferedInputStream(a);
+        try{
+            Player p = new Player(b);
+           // p.play();
+            p.play(200);
+        }catch(JavaLayerException ex){};
+    }catch(IOException e){};  
     }
     
 }
