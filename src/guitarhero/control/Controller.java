67aecaf6ac21637ac5a766514916fccba43dcc9d
BUDDY_1cent <BUDDY_1cent@192.168.0.102>
2014-10-18 20:01:46 +0200

    new class StopWatch new in Controller

--- src/guitarhero/control/Controller.java
+++ src/guitarhero/control/Controller.java
@@ -7,8 +7,9 @@ package guitarhero.control;
 
 import guitarhero.view.GuitarHeroView;
 import guitarhero.models.*;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 /**
  *
@@ -17,6 +18,8 @@ import java.util.List;
 public class Controller {
     private Song song;
     private GuitarHeroView view; 
+    private List<Dot> playingDots;
+    private StopWatch stopWatch;
     
     public void initializeGame(){
         //nacteni tecek v Song
@@ -25,6 +28,8 @@ public class Controller {
     }
     
     public Controller(){
+        playingDots = new ArrayList<>();
+        stopWatch = new StopWatch();
     }
 
     private Thread thread;
@@ -37,16 +42,33 @@ public class Controller {
     public void start() {
         initializeGame();
         running = true;
+        stopWatch.start();
         view.updateModel(song.getDots());
         thread = new Thread() {
             @Override
             public void run() {
                 while (running) {
-                    for(Dot d: song.getDots())
+                    for(Dot dot: song.getDots()){ //zkouska: prochazeni vsech tecek (je tam jedna) a pokud tecka dosahne 400 zastavi stopky
+                        
+                        if(stopWatch.getElapsedTime() >= dot.getTime() - 2000 && stopWatch.getElapsedTime() < dot.getTime() - 1950 ){
+                            playingDots.add(dot);
+                            System.out.println("hejhej");
+                        }
+                        if(stopWatch.getElapsedTime() > dot.getTime()){
+                            playingDots.remove(dot);
+                            stopWatch.stop();
+                        }
+                    }
+                    System.out.println("Cas stopek: "+stopWatch.getElapsedTime()); //Pomocny vypis stopek
+                    for(Dot d: playingDots){
                         d.move();
+                        System.out.println("Mam tecku");
+                    }
+                    System.out.println("hej");
                     view.repaint();
+
                     try {
-                        Thread.sleep(20);
+                        Thread.sleep(10);
                     } catch (InterruptedException ex) {
                    }
                 }
---
+++ src/guitarhero/control/StopWatch.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitarhero.control;
+/**
+ *
+ * @author BUDDY_1cent
+ */
+public class StopWatch {
+
+  private long startTime = 0;
+  private long stopTime = 0;
+  private boolean running = false;
+
+  public void start() {
+    this.startTime = System.currentTimeMillis();
+    this.running = true;
+  }
+
+  public void stop() {
+    this.stopTime = System.currentTimeMillis();
+    this.running = false;
+  }
+
+  //elaspsed time in milliseconds
+  public long getElapsedTime() {
+    long elapsed;
+    if (running) {
+      elapsed = (System.currentTimeMillis() - startTime);
+    }
+    else {
+      elapsed = (stopTime - startTime);
+    }
+    return elapsed;
+  }
+}
